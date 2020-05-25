d20b24868c16df554355f39319d5ab6423ac97cd
Kevin Rubin <krubin@highpoint.edu>
2020-05-25 11:25:56 -0400

    Converts each line of json string into a python dictionary

--- read-log-file.py
+++ read-log-file.py
@@ -4,13 +4,24 @@ import json
 cowrie-log-analyzer: Read Log File
 
 '''
+#As more files will be added, this will be useful
 #if __name__ == '__main__':
 
 fileName = "cowrie.json.2020-03-19"
 file = open(fileName, "r")
-#print(file.read())
 
+
+#Gets the lines from the log file
 lines = []
 lines = file.readlines()
 
-print(lines)
+
+#Converts each line from string of json to a python dictionary
+#then puts each line into a list
+json_list = []
+
+for line in lines:
+	json_line_dict = json.loads(line)
+	json_list.append(json_line_dict)
+
+print(json_list)
