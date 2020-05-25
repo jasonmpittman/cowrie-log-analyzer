0d4cb162759100009eb64ab2f9a821dff46ad398
Kevin Rubin <krubin@highpoint.edu>
2020-05-25 11:33:09 -0400

    print eventid

--- read-log-file.py
+++ read-log-file.py
@@ -7,14 +7,13 @@ cowrie-log-analyzer: Read Log File
 #As more files will be added, this will be useful
 #if __name__ == '__main__':
 
+#Opens the file
 fileName = "cowrie.json.2020-03-19"
 file = open(fileName, "r")
-
 
 #Gets the lines from the log file
 lines = []
 lines = file.readlines()
-
 
 #Converts each line from string of json to a python dictionary
 #then puts each line into a list
@@ -24,4 +23,6 @@ for line in lines:
 	json_line_dict = json.loads(line)
 	json_list.append(json_line_dict)
 
-print(json_list)
+print("eventid")
+for json_dict in json_list:
+	print(json_dict["eventid"])
