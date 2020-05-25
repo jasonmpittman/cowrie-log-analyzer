3e466700ba46ad723400f3dec188fe98ae5633f2
Kevin Rubin <krubin@highpoint.edu>
2020-05-25 11:05:52 -0400

    Puts each line of the file into a list

--- read-log-file.py
+++ read-log-file.py
@@ -1,10 +1,16 @@
+import json
+
 '''
 cowrie-log-analyzer: Read Log File
 
 '''
-
-
 #if __name__ == '__main__':
+
 fileName = "cowrie.json.2020-03-19"
 file = open(fileName, "r")
-print(file.read())
+#print(file.read())
+
+lines = []
+lines = file.readlines()
+
+print(lines)
