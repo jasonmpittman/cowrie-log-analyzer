0a38f33d3c63d35d973cd9db56f9c59d34316170
Kevin Rubin <krubin@highpoint.edu>
2020-06-05 12:47:19 -0400

    UI: Created Scroll (text) Section class

--- ui_elements.py
+++ ui_elements.py
@@ -15,3 +15,21 @@ class standardButton:
 
 	def Pack(self, Side, px=10, py=10):
 		self.button.pack(side=Side, padx=px, pady=py)
+
+class ScrollSection:
+	def __init__(self, parent, h, w, title, px=10, py=10):
+		self.scrollFrame = tk.Frame(parent, bg="white", bd=5, relief="groove", width=w, height=h)
+		self.title = tk.Label(self.scrollFrame, text=title)
+		self.title.pack(side="top", padx=10, pady=2)
+		self.scroll = tk.Scrollbar(self.scrollFrame)
+		self.scrollText = tk.Text(self.scrollFrame, yscrollcommand=self.scroll.set, height=h, width=w)
+		self.scrollText.pack(side="left", padx=5, pady=10)
+
+	def Grid(self, r, c, px=10, py=10):
+		self.scrollFrame.grid(row=r, column=c, padx=px, pady=py)
+
+	def Pack(self, Side, px=10, py=10):
+		self.scrollFrame.pack(side=Side, padx=px, pady=py)
+
+	def Append(self, text):
+		self.scrollText.insert(tk.END, text + "\n")
