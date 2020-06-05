87820d43c939fcd48c0dee73d76745aca6a4077e
Kevin Rubin <krubin@highpoint.edu>
2020-06-05 17:10:14 -0400

    UI: Created Graph class for visualizing data

--- ui_elements.py
+++ ui_elements.py
@@ -4,7 +4,14 @@ from PIL import ImageTk, Image
 
 import sys
 
-from matplotlib import pyplot as plt
+import matplotlib
+
+matplotlib.use("TkAgg")
+
+from matplotlib.figure import Figure
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 class standardButton:
 	def __init__(self, parent, commandFunction, buttonText):
@@ -33,3 +40,37 @@ class ScrollSection:
 
 	def Append(self, text):
 		self.scrollText.insert(tk.END, text + "\n")
+
+class Graph:
+	def __init__(self, parent, widthIn, heightIn, xLabel, yLabel, title="Title"):
+		self.data = []
+		self.figure = Figure(figsize=(widthIn, heightIn), dpi=100)
+		self.graph = self.figure.add_subplot(1, 1, 1, label=title)
+		self.canvas = FigureCanvasTkAgg(self.figure, parent)
+		self.Widget = self.canvas.get_tk_widget()
+		self.title = title
+		self.xLabel = xLabel
+		self.yLabel = yLabel
+		# xlabel=axisLabel[0], ylabel=axisLabel[1]
+
+	def Grid(self, r, c, colSpan=2, px=10, py=10):
+		self.Widget.grid(row=r, column=c, columnspan=colSpan, padx=px, pady=py)
+
+	def Pack(self, Side, px=10, py=10):
+		self.Widget.pack(side=Side, padx=px, pady=py)
+
+	def Data(self, data): #add labels as well
+		if len(data) < 2 or not isinstance(data[0], list):
+			print("Needs more than one axis in order to draw the graph")
+			return
+		self.data = data
+
+	def draw(self):
+		self.graph.cla()
+		xAxis = self.data.pop(0)
+		for axis in self.data:
+			self.graph.plot(xAxis)
+		self.graph.set_title(self.title)
+		self.graph.set_xlabel(self.xLabel)
+		self.graph.set_ylabel(self.yLabel)
+#
