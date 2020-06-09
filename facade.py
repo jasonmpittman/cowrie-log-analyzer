57e926a36a2feff755b6876f17ea5d7ebd861403
Kevin Rubin <krubin@highpoint.edu>
2020-06-09 17:18:57 -0400

    UI: Basic UI elements placed and a pop out Graph window made

--- facade.py
+++ facade.py
@@ -2,6 +2,15 @@ from events_class import *
 
 from ui_elements import *
 
+import sys
+
+def exitFunction():
+	sys.exit()
+
+def graphWindow():
+	graphW = tk.Toplevel(root)
+	graphW.title("Graph")
+
 root = tk.Tk()
 root.title("Cowrie Log Analyzer")
 root.configure()
@@ -25,5 +34,15 @@ Passwords.Grid(0, 3)
 IP_Address.Append(E.topTen("src_ip"))
 User_names.Append(E.topTen("username"))
 Passwords.Append(E.topTen("password"))
+
+BottomBar = tk.Frame(root)
+BottomBar.grid(row=1, column=3, sticky="s")
+
+Exit = standardButton(BottomBar, exitFunction , "Exit")
+Exit.Pack("right")
+
+Graph = standardButton(BottomBar, graphWindow, "Graph")
+Graph.Pack("right")
+
 
 root.mainloop()
