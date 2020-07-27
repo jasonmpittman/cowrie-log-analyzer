c88e92dc5853a72a8eadb16c5cbdc00f2bca8738
Kevin Rubin <kar3154@gmail.com>
2020-07-27 14:06:22 -0400

    added docstring to facade

--- facade.py
+++ facade.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+
+__author__ = "Kevin A. Rubin, Jason M. Pittman"
+__copyright__ = "Copyright 2020"
+__credits__ = ["Kevin A. Rubin, Jason M. Pittman"]
+__license__ = "GPLv3"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__email__ = "jpittman@highpoint.edu"
+__status__ = "Release"
+__dependecies__ = "system, log, tkinter"
+
 import system
 import log
 import tkinter as tk
@@ -5,28 +17,55 @@ import tkinter as tk
 facade_logger = log.Logger("facade")
 
 class event_handler:
+	"""
+	This acts as an intermediary between the actual functionality of the buttons and the UI component.
+
+	Attributes
+	----------
+	logic : logic object
+	update_screen : function
+	export : function
+	_alert_window : alert_window object
+	"""
 	def __init__(self, update_screen, export, alert_window):
+		"""
+		Parameters
+		----------
+		update_screen : function
+		export : function
+		alert_window : alert_window object
+		"""
 		self.logic = system.logic()
 		self.update_screen = update_screen
 		self.export = export
 		self._alert_window = alert_window
 		facade_logger.info(self.__class__.__name__, self.__init__.__name__, "Initialized")
 
-	'''
-	All of these functions are handed over to to buttons to be called. The buttons call these functions.
-	Requirements: Must return True or False in the context of the function being successful in its execution or not successful.
-	'''
 	def import_pop_get_data(self, name):
+		"""
+		Calls get data function in system.py, must return true or false based on success or failure.
+
+		Parameters
+		----------
+		name : str
+		"""
 		facade_logger.info(self.__class__.__name__, self.import_pop_get_data.__name__, "import_pop_get_data running")
 		return self.logic.get_data(name)
 
 	def import_pop_update_database(self):
+		"""
+		Updates database and then updates the screen. Returns True
+		"""
 		facade_logger.info(self.__class__.__name__, self.import_pop_update_database.__name__, "import_pop_update_database running")
 		res = self.logic.update_database()
 		self.update_screen()
 		return True
 
 	def import_files(self):
+		"""
+		Uses default os file selector in order to import files.
+		Provides user with alerts based on if there are import issues.
+		"""
 		filenames = tk.filedialog.askopenfilenames(initialdir="/import")
 		if filenames == "":
 			print("Cancel Selected!")
@@ -46,15 +85,28 @@ class event_handler:
 
 
 	def export_pop_export_data(self, filename):
+		"""
+		Calls the export function.
+
+		Parameters
+		----------
+		filename : str
+		"""
 		facade_logger.info(self.__class__.__name__, self.export_pop_export_data.__name__, "export_pop_export_data running")
 		return self.export(filename)
 
 	def export_pop_no_update(self):
+		"""
+		Calls the no_update function, then returns True
+		"""
 		facade_logger.info(self.__class__.__name__, self.export_pop_no_update.__name__, "export_pop_no_update running")
 		self.logic.no_update()
 		return True
 
 	def exit_button_press(self):
+		"""
+		Exits out of the program and then returns True.
+		"""
 		facade_logger.info(self.__class__.__name__, self.exit_button_press.__name__, "exit_button_press running")
 		self.logic.exit()
 		return True
