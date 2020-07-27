d0119fa6da5eb14f47924796bfec032e14306169
Kevin Rubin <kar3154@gmail.com>
2020-07-27 13:46:53 -0400

    events_database.py

--- events_database.py
+++ events_database.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+
+__author__ = "Kevin A. Rubin, Jason M. Pittman"
+__copyright__ = "Copyright 2020"
+__credits__ = ["Kevin A. Rubin, Jason M. Pittman"]
+__license__ = "GPLv3"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__email__ = "jpittman@highpoint.edu"
+__status__ = "Release"
+__dependecies__ = "sqlite3, json, datetime, country_database, log"
+
 import sqlite3
 
 from sqlite3 import Error
@@ -6,14 +18,21 @@ import json
 
 import datetime
 
-import log
-
 import country_database
+
+import log
 
 events_db_log = log.Logger("events_db")
 
-'''Creates a database connection to a SQLite database '''
+
 def create_connection(db_file="events.db"):
+	"""
+	Creates a database connection to a SQLite database (specifically used for events.db)
+
+	Parameters
+	----------
+	db_file : str
+	"""
 	conn = None
 	try:
 		conn = sqlite3.connect(db_file)
@@ -29,20 +48,32 @@ def create_connection(db_file="events.db"):
 
 
 '''
-Creates the command table
+
 '''
 def create_command_table(conn):
+	"""
+	Creates the command table if it does not exist
+
+	Parameters
+	----------
+	conn : database connection structure
+	"""
 	create_command_sql = """CREATE TABLE IF NOT EXISTS commands (
 						foreign_key INTEGER,
 						command TEXT);
 						"""
 	run_sql(conn, create_command_sql)
 
-'''
-Input: conn --> connection, sql --> sql query
-Output: res --> results of the query
-'''
 def run_sql(conn, sql):
+	"""
+	Input: conn --> connection, sql --> sql query
+	Output: res --> results of the query
+
+	Parameters
+	----------
+	conn : database connection structure
+	sql : str
+	"""
 	try:
 		c = conn.cursor()
 		#executes the phrase
@@ -54,11 +85,15 @@ def run_sql(conn, sql):
 	except Error as e:
 		print(e)
 
-'''
-Creates the events table
-Input: conn --> a connection
-'''
 def create_table(conn):
+	"""
+	Creates the events table if it does not already exist
+	Input: conn --> a connection
+
+	Parameters
+	----------
+	conn : database connection structure
+	"""
 	create_table_sql = """CREATE TABLE IF NOT EXISTS events (
 						type TEXT,
 						ip_address TEXT,
@@ -74,22 +109,28 @@ def create_table(conn):
 
 	run_sql(conn, create_table_sql)
 
-'''
-Loads the configuration file (json) into a python dictionary
-Output: config_dict --> a dictionary
-'''
+
 def get_config():
+	"""
+	Loads the configuration file (json) into a python dictionary
+	Output: config_dict --> a dictionary
+	"""
 	with open("config/types.config.json", "r") as config_file:
   		config = config_file.read()
 	config_dict = json.loads(config)
 	return config_dict
 
-'''
-Uses the configuration dictionary to categorize the type of event
-Input: event_id
-Outputs: categorization
-'''
+
 def get_type(event_id):
+	"""
+	Uses the configuration dictionary to categorize the type of event
+	Input: event_id
+	Outputs: categorization
+
+	Parameters
+	----------
+	event_id : str
+	"""
 	config_dict = get_config()
 	try:
 		return config_dict[event_id]
@@ -98,23 +139,33 @@ def get_type(event_id):
 		events_db_log.info("", get_type.__name__, f"Missing from config file: {event_id}, full event id will be used")
 		return event_id
 
-'''
-Takes in a file_path and gets the actual file name from the path
-Input: file path
-Output: file name
-'''
+
 def get_file_name(file_path):
+	"""
+	Takes in a file_path and gets the actual file name from the path
+	Input: file path
+	Output: file name
+
+	Parameters
+	----------
+	file_path : str
+	"""
 	if file_path == "-":
 		return "-"
 	path = file_path.split("/")
 	return path[-1]
 
-'''
-Takes in an event and prepares the data before returning it as a touple for sql
-Input: event
-Output: touple of all event information
-'''
+
 def get_data_touple(event):
+	"""
+	Takes in an event and prepares the data before returning it as a touple for sql
+	Input: event
+	Output: touple of all event information
+
+	Parameters
+	----------
+	event : event object
+	"""
 	type = get_type(event.event["eventid"])
 	ip = event.get("src_ip")
 	username = event.get("username")
@@ -133,22 +184,33 @@ def get_data_touple(event):
 	message = event.get("message")
 	return (type, ip, username, password, filename, country, duration, date_time, message)
 
-'''
-Splits a line of commands into their compenents
-Input: command_line -> a line with commands
-Output: command_line -> list of commands
-'''
 def get_command_list(command_line):
+	"""
+	Splits a line of commands into their compenents
+	Input: command_line -> a line with commands
+	Output: command_line -> list of commands
+
+	Parameters
+	----------
+	command_line : str
+	"""
 	command_line = command_line.replace("CMD: ", "")
 	command_line = command_line.split("; ")
 	return command_line
 
-'''
-Inserts and event into the events database
-Input: conn, event
-Output: rowid -> unique row identifier
-'''
+
 def add_event(conn, event):
+	"""
+	Inserts and event into the events database
+	Input: conn, event
+	Output: rowid -> unique row identifier
+
+	Parameters
+	----------
+	conn : database connection
+	event : event object
+	"""
+
 	sql = """INSERT INTO events(type, ip_address, username, password, filename, country, duration, timestamp, message) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)  """
 	preped_data = get_data_touple(event)
 	cur = conn.cursor()
@@ -180,15 +242,20 @@ def add_event(conn, event):
 				return False
 	return True
 
-'''
-Returns a string that lists the top 10 most frequent data for a given column and the #1 (these are returned as a touple)
-Input: conn, col1 --> a column name to run top 10 on
-Output: (strReturn -> top 10 string, first -> #1)
-'''
-def query_top_ten(conn, col1):
-	sql = f"""SELECT {col1},COUNT({col1}) AS cnt FROM events
-			WHERE {col1} NOT IN ('-')
-			GROUP BY {col1}
+def query_top_ten(conn, col):
+	"""
+	Returns a string that lists the top 10 most frequent data for a given column and the #1 (these are returned as a touple)
+	Input: conn, col --> a column name to run top 10 on
+	Output: (strReturn -> top 10 string, first -> #1)
+
+	Parameters
+	----------
+	conn : database connection structure
+	col : str
+	"""
+	sql = f"""SELECT {col},COUNT({col}) AS cnt FROM events
+			WHERE {col} NOT IN ('-')
+			GROUP BY {col}
 			ORDER BY cnt DESC;"""
 
 	cur = conn.cursor()
@@ -201,7 +268,7 @@ def query_top_ten(conn, col1):
 		strReturn = ""
 		stop_val = 10
 		print_num = 1
-		events_db_log.info("", query_top_ten.__name__, f"Top ten queried {col1}")
+		events_db_log.info("", query_top_ten.__name__, f"Top ten queried {col}")
 		while i < stop_val and i < len(res):
 			key, _ = res[i]
 
@@ -214,18 +281,23 @@ def query_top_ten(conn, col1):
 			i += 1
 		first, _ = res[0]
 		cur.close()
-		events_db_log.info("", query_top_ten.__name__, f"Top ten queried {col1} - Successful")
+		events_db_log.info("", query_top_ten.__name__, f"Top ten queried {col} - Successful")
 		return (strReturn, first)
 	except:
 		print("Failed")
 		return None
 
-'''
-A special function that does the same as the other top ten but customized for username and password pair
-Input: conn
-Output: strReturn -> top 10 string, first -> #1
-'''
+
 def top_ten_user_pass(conn):
+	"""
+	A special function that does the same as the other top ten but customized for username and password pair
+	Input: conn
+	Output: strReturn -> top 10 string, first -> #1
+
+	Parameters
+	----------
+	conn : database connection structure
+	"""
 	sql = f"""SELECT username, password FROM events WHERE username NOT IN ('-') and password NOT IN ('-');"""
 	cur = conn.cursor()
 	i = 0
@@ -255,11 +327,12 @@ def top_ten_user_pass(conn):
 	first, val = sortedDictionary[0]
 	return strReturn, first
 
-'''
-Gets the top 10 and #1 longest duration (not by frequency)
-Output: (output_str -> top 10 string, first -> #1)
-'''
+
 def longest_durations():
+	"""
+	Gets the top 10 and #1 longest duration (not by frequency)
+	Output: (output_str -> top 10 string, first -> #1)
+	"""
 	output_str = ""
 	sql = """SELECT duration FROM events
 			WHERE duration NOT IN ('-')
@@ -278,7 +351,3 @@ def longest_durations():
 		i += 1
 	events_db_log.info("", longest_durations.__name__, f"Top ten longest durations - Successful")
 	return output_str, first
-
-
-if __name__ == '__main__':
-	pass
