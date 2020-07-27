6ea59d2b2bfce8b7b2c82f58c3ac098f2bd66544
Kevin Rubin <kar3154@gmail.com>
2020-07-27 13:13:20 -0400

    added decstring to event_class.py

--- event_class.py
+++ event_class.py
@@ -1,15 +1,31 @@
 class Event:
+	"""
+	This is the event class which holds the data for an event
+	Properties
+	----------
+	event : python dictionary
+	"""
 	def __init__(self, json):
+		"""
+		Parameters
+		----------
+		json : python dictionary
+		"""
 		self.event = json
 
 	def printEvent(self):
 		print(self.event)
 
 	'''
-	returns the value for the given key or "-" if it does not have one
+	Get function:
+	returns the value for the given key or "-" if it does not have one, otherwise it returns the event key
 	'''
-
 	def get(self, key):
+		"""
+		Parameters
+		----------
+		key : str
+		"""
 		try:
 			return self.event[key]
 		except:
