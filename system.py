5fb43f53ce2de959a264405237f2bdda6e18b93d
Kevin Rubin <kar3154@gmail.com>
2020-07-27 15:06:02 -0400

    added docstring to system.py

--- system.py
+++ system.py
@@ -1,19 +1,54 @@
+#!/usr/bin/env python3
+
+__author__ = "Kevin A. Rubin, Jason M. Pittman"
+__copyright__ = "Copyright 2020"
+__credits__ = ["Kevin A. Rubin, Jason M. Pittman"]
+__license__ = "GPLv3"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__email__ = "jpittman@highpoint.edu"
+__status__ = "Release"
+__dependecies__ = "events_class, events_database, sys, log"
+
 import events_class
+
 import events_database
+
 import sys
+
 import log
 
 system_logger = log.Logger("system")
 
 class logic:
+	"""
+	This class holds functionality code that the facade calls.
+
+	Attributes
+	----------
+	events_object : events_class object
+	"""
 	def __init__(self):
 		self.events_object = events_class.Events()
 
 	def get_data(self, name):
+		"""
+		Calls the get_data function in the events class.
+
+		Parameters
+		----------
+		name : str
+		"""
 		system_logger.info(self.__class__.__name__, self.get_data.__name__, "get_data called")
 		return self.events_object.get_data(name)
 
 	def update_database(self):
+		"""
+		Updates the events database with what is in the events object.
+		returns the a touple of test and count
+		test : determins if any event failed to be inserted
+		count : number of failed inserts (usually due to repeate data)
+		"""
 		system_logger.info(self.__class__.__name__, self.update_database.__name__, "update_database called")
 		conn = events_database.create_connection("events.db")
 		config_dict = events_database.get_config()
@@ -34,5 +69,8 @@ class logic:
 		pass
 
 	def exit(self):
+		"""
+		Calls the sys.exit function
+		"""
 		system_logger.info(self.__class__.__name__, self.exit.__name__, "exit called")
 		sys.exit()
